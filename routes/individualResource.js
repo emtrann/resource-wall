9d2faa173c8d3ba77d57c09553fc41a06933e9af
Michelle Tran <michelletraan@hotmail.com>
2020-10-17 23:53:00 +0000

    Add routes and individual view pages

---
+++ routes/individualResource.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (db) => {
+  router.get("/:individualresource", (req, res) => {
+    console.log('this is the individual resource page')
+  })
+
+  return router;
+}
