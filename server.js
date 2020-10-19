936f0f7977f4c4fdb179490a824cc2623c85539a
Al Lotina <lotina.al@gmail.com>
2020-10-19 22:43:19 +0000

    added temporary database

--- server.js
+++ server.js
@@ -10,11 +10,15 @@ const sass       = require("node-sass-middleware");
 const app        = express();
 const morgan     = require('morgan');
 
+// AL added
+const bcrypt = require('bcrypt');
+// const cookieSession = require('cookie-session');
+
 // PG database client/connection setup
-const { Pool } = require('pg');
-const dbParams = require('./lib/db.js');
-const db = new Pool(dbParams);
-db.connect();
+// const { Pool } = require('pg');
+// const dbParams = require('./lib/db.js');
+// const db = new Pool(dbParams);
+// db.connect();
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -31,6 +35,30 @@ app.use("/styles", sass({
 }));
 app.use(express.static("public"));
 
+// AL added below
+// app.use(cookieSession({
+//   name: 'session',
+//   keys: ['asdf', 'lkjhg']
+// }));
+// Temporary Data to be replaced by Database:
+const resourcesDatabase = {
+  1: { URL: 'https://business.tutsplus.com/tutorials/how-to-start-a-business--cms-25638', title: 'Awesome business tutorial!', description: 'All you need to know to start yout own company', userID: "user1" },
+  2: { URL: 'https://www.khanacademy.org/science/high-school-physics', title: 'Physics 101', description: 'The best way to learn physics!', userID: "user1" },
+  3: { URL: 'https://www.freecodecamp.org/news/free-online-programming-cs-courses/', title: 'Software development tutorial', description: 'Well explain software development intro', userID: "user2" }
+};
+const users = {
+  "user1": {
+    id: "user1",
+    email: "user1@example.com",
+    password: bcrypt.hashSync("password", 10)
+  },
+  "user2": {
+    id: "user2",
+    email: "user2@example.com",
+    password: bcrypt.hashSync("password", 10)
+  }
+};
+
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
@@ -44,23 +72,43 @@ const categories = require("./routes/categories");
 // NOT ACTUALLY SURE WHAT THIS DOES HERE -m
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
-app.use("/api/users", usersRoutes(db));
-// Note: mount other resources here, using the same pattern above
-app.use("/homepage", homepageRoutes(db));
-app.use("/resource/:individualresource", individualResourceRoutes(db));
-app.use("/newresource", newResourceRoutes(db));
-app.use("/register", register(db));
-app.use("/category/:categoryID", categories(db));
+// app.use("/api/users", usersRoutes(db));
+// // Note: mount other resources here, using the same pattern above
+// app.use("/homepage", homepageRoutes(db));
+// app.use("/resource/:individualresource", individualResourceRoutes(db));
+// app.use("/newresource", newResourceRoutes(db));
+// app.use("/register", register(db));
+// app.use("/category/:categoryID", categories(db));
 
 // UP UNTIL HERE
+
+// AL added below:
+// returns an object containing all resouces for a given userID:
+const resourcesForUser = function(database, id) {
+  const filteredResources = {};
+  for (let resourceId in database) {
+    const resourceObj = database[resourceId];
+    if (resourceObj.userID === id) {
+      filteredResources[resourceId] = resourceObj;
+    }
+  }
+  return filteredResources;
+};
 
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 
 // get root directory - evenutally should be page of resources for guest users
-app.get("/", (req, res) => {
-  res.render("index");
+
+// AL added below:
+app.get('/', (req, res) => {
+  // if (!req.session.user_id) {
+  //   res.redirect('/login');
+  // }
+  const userObject = resourcesForUser(resourcesDatabase, 'user1'); // id hardcoded for now
+  const templateVars = { resources: userObject }; //, user: 'user1' }; // ?
+  res.render('index', templateVars);
 });
 
 // homepage for users - redirect here after login + shows liked & saved resources
