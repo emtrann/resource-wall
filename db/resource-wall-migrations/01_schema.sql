f76118509e870d7357d68cc38f7ad57ecea65fcf
Al Lotina <lotina.al@gmail.com>
2020-10-23 01:57:05 +0000

    master update

--- db/resource-wall-migrations/01_schema.sql
+++ db/resource-wall-migrations/01_schema.sql
@@ -8,10 +8,6 @@ CREATE TABLE users (
   password VARCHAR(255) NOT NULL
 );
 CREATE TABLE categories (
-<<<<<<< HEAD
-  id SERIAL,
-=======
   id SERIAL ,
->>>>>>> newresource-page
   name VARCHAR(255) PRIMARY KEY NOT NULL
 );
