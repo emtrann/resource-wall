720b68cf93f0e8f2b9a9656714c29f91a50a0bf7
Al Lotina <lotina.al@gmail.com>
2020-11-03 19:52:27 +0000

    last master update

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,34 +1,19 @@
 $(() => {
-/*  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-    if (!user){
-      $('.error').slideToggle(1000);
-    }
-  });;
-  */
   $('#login-form').submit( (event) => {
-    //console.log("Any text");
-    event.preventDefault();
-    $.ajax({
-      method: "POST",
-      url: "/",
-      data: $(event.target).serialize(),
-    }).done((res) => {
-      console.log(res);
-      if (res.success) {
-        console.log("homepage");
-        window.location="/homepage";
-      } else {
-        $('.error').slideToggle(1000);
-      }
-    });;
-  })
-});
-
-
-
+      //console.log("Any text");
+      event.preventDefault();
+      $.ajax({
+        method: "POST",
+        url: "/",
+        data: $(event.target).serialize(),
+      }).done((res) => {
+        console.log(res);
+        if (res.success) {
+          console.log("homepage");
+          window.location="/homepage";
+        } else {
+          $('.error').slideToggle(1000);
+        }
+      });;
+    })
+  });
